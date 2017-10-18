22cc98c7626f0213435844ce0a3a1e3897b25898
W4ld3n <malte_sahrhage@web.de>
2017-10-18 15:10:18 +0200

    R functions and  Test cases

---
+++ R/template_function.R
@@ -0,0 +1,10 @@
+#' Useless String machine!
+#' Return a default message.
+#'
+#' Comments for roxygen2 are useful for documentation
+#' @param text Text to be printed out.
+#' @export
+#' @return Input text
+template_function = function(text="Hello, Template!"){
+  return(text);
+}
---
+++ R/template_test.R
@@ -0,0 +1,16 @@
+#Using the testthat library for R Unit testing
+library("testthat")
+
+#For using it in a package, use devtools::use_testthat()
+#That will initialize the tests/testhat dir and create
+#a testthat.R file in tests that executes all the tests
+#on R CMD Check
+
+#Message when executing R CMD Check or devtools::test()
+context("Very importatnt test")
+
+#First Test-Case
+test_that("This should better be working!",{
+  x = c(1,2,3)
+  expect_that(length(x),equals(3))
+})
