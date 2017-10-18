bd9d80638ee9b073e03bd45281624b7914a72aed
W4ld3n <malte_sahrhage@web.de>
2017-10-18 13:57:49 +0200

    Adding Arduino,Processing templates

---
+++ Arduino/template.ino
@@ -0,0 +1,10 @@
+//Startup Code
+void setup(){
+  //Set Baud Rate to 9600
+  Serial.begin(9600);
+}
+
+//Execute the code loop
+void loop(){
+
+}
---
+++ Processing/template.pde
@@ -0,0 +1,10 @@
+//Code at Startup
+void setup() {
+
+}
+
+
+//Loop for continous code execution
+void draw(){
+
+}
